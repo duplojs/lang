fffbfa8590fb00741aadc96b4071000619d19533
ZeRiix <willflo46@gmail.com>
2026-07-22 17:42:13 +0200

    fix(hf): number min and max

--- scripts/number/min.ts
+++ scripts/number/min.ts
@@ -2,7 +2,7 @@ import type * as DArray from "@scripts/array";
 import type * as DCommon from "@scripts/common";
 
 export function min<
-	GenericArray extends DCommon.AnyTuple<number> | readonly number[] & DArray.MinElements<1>,
+	GenericArray extends DCommon.AnyTuple<number> | readonly number[] & DArray.MinElements<number>,
 >(array: GenericArray) {
 	// Use a loop if spread inputs can become large.
 	return Math.min(...array);
